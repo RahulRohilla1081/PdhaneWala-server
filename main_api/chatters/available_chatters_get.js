020a6a34d92c06c8f5dc63cd6408304248d9c056
Joyeeta B Panda <joyeeta.b@samishti.com>
2024-01-26 19:52:20 +0530

    available chatters socket corrected

--- main_api/chatters/available_chatters_get.js
+++ main_api/chatters/available_chatters_get.js
@@ -11,87 +11,82 @@ http://localhost:9000/main_api/chatters/available_chatters_get?CUSTOMER_ID=PWC00
 
 router.get("/", async function (req, res, next) {});
 
-const saveChatData = async (data) => {
+const availableChatters = async (data) => {
+  var returnData;
+
   try {
     let CUSTOMER_ID = data.CUSTOMER_ID;
     let USER_ID = data.USER_ID;
     let db = await dbConnect();
 
-    var returnData;
+    const response = await axios.get(
+      "http://localhost:9000/main_api/chatters/chatters_get?CUSTOMER_ID=" +
+        CUSTOMER_ID +
+        "&USER_ID=" +
+        USER_ID
+    );
+    // console.log("response.data", response.data);
+    let chatterData = { ...response.data };
+    let chatData = await db
+      .collection("chat")
+      .find({
+        $or: [
+          { sender_customer_id: CUSTOMER_ID, sender_id: USER_ID },
+          { receiver_customer_id: CUSTOMER_ID, receiver_id: USER_ID },
+        ],
+      })
+      .toArray();
+    // console.log("chatData", chatData[0]);
 
-    axios
-      .get(
-        "http://localhost:9000/main_api/chatters/chatters_get?CUSTOMER_ID=" +
-          CUSTOMER_ID +
-          "&USER_ID=" +
-          USER_ID
-      )
-      .then(async (response) => {
-        let chatterData = { ...response.data };
-        let chatData = await db
-          .collection("chat")
-          .find({
-            $or: [
-              { sender_customer_id: CUSTOMER_ID, sender_id: USER_ID },
-              { receiver_customer_id: CUSTOMER_ID, receiver_id: USER_ID },
-            ],
-          })
-          .toArray();
-        console.log("chatterData", chatterData);
-        console.log("chatData", chatData);
+    let available_chatter = [];
+    if (chatData.length > 0) {
+      if (chatterData.student.length > 0) {
+        chatterData.student.map((val) => {
+          if (
+            chatData.some(
+              (item) =>
+                (item.sender_customer_id == val.CUSTOMER_ID &&
+                  item.sender_id == val.USER_ID) ||
+                (item.receiver_customer_id == val.CUSTOMER_ID &&
+                  item.receiver_id == val.USER_ID)
+            )
+          ) {
+            // console.log("chatterData", chatterData);
+            available_chatter.push(val);
+          }
+        });
+      }
+      if (chatterData.teachers.length > 0) {
+        chatterData.teachers.map((val) => {
+          if (
+            chatData.some(
+              (item) =>
+                (item.sender_customer_id == val.CUSTOMER_ID &&
+                  item.sender_id == val.USER_ID) ||
+                (item.receiver_customer_id == val.CUSTOMER_ID &&
+                  item.receiver_id == val.USER_ID)
+            )
+          ) {
+            available_chatter.push(val);
+          }
+        });
+      }
+      returnData = {
+        message: "chat history available",
+        data: available_chatter,
+      };
+    } else {
+      returnData = { message: "No chat history", data: [] };
+    }
 
-        let available_chatter = [];
-        if (chatData.length > 0) {
-          if (chatterData.student.length > 0) {
-            chatterData.student.map((val) => {
-              if (
-                chatData.some(
-                  (item) =>
-                    (item.sender_customer_id == val.CUSTOMER_ID &&
-                      item.sender_id == val.USER_ID) ||
-                    (item.receiver_customer_id == val.CUSTOMER_ID &&
-                      item.receiver_id == val.USER_ID)
-                )
-              ) {
-                available_chatter.push(val);
-              }
-            });
-          }
-          if (chatterData.teachers.length > 0) {
-            chatterData.teachers.map((val) => {
-              if (
-                chatData.some(
-                  (item) =>
-                    (item.sender_customer_id == val.CUSTOMER_ID &&
-                      item.sender_id == val.USER_ID) ||
-                    (item.receiver_customer_id == val.CUSTOMER_ID &&
-                      item.receiver_id == val.USER_ID)
-                )
-              ) {
-                available_chatter.push(val);
-              }
-            });
-          }
-          returnData = {
-            message: "chat history available",
-            data: available_chatter,
-          };
-          // res.send({
-          //   message: "chat history available",
-          //   data: available_chatter,
-          // });
-        } else {
-          // res.send({ message: "No chat history", data: [] });
-          returnData = { message: "No chat history", data: [] };
-        }
-      });
+    // return returnData;
+    return Promise.resolve(returnData);
   } catch (err) {
-    console.log(err);
-    axios_function_all_APIs_catch(__filename, res.statusCode, req.query);
-    // res.send({ message: "Error in " + __filename });
-    returnData = { message: "Error in " + __filename };
+    let returnData = { message: "Error" + __filename };
+    return Promise.reject(returnData);
+
+    // return returnData;
   }
-  return returnData;
 };
 
 module.exports = {
@@ -101,21 +96,26 @@ module.exports = {
     io.sockets.on("connection", (socket) => {
       console.log("Connection success", socket.id);
 
-      // socket.on("joinUser", function (data) {
-      //   console.log("jahsgjasgj", data);
-      //   if (data.AUTH_ID != undefined && data.CUSTOMER_ID != undefined) {
-      //     socket.join(data.AUTH_ID + data.CUSTOMER_ID);
-      //   }
-      // });
-
       socket.on("availableChatterGet", async (data) => {
-        console.log("chatPayload", data);
-        await saveChatData(chatPayload),
-          io.sockets
-            .in(data.USER_ID + data.CUSTOMER_ID)
-            .emit("availableChatterData", {
-              CHATTER_DATA: await saveChatData(data),
-            });
+        console.log("jdfjfjgj", data);
+        availableChatters(data)
+          .then((response) => {
+            console.log("asdasda", response);
+            io.sockets
+              .in(data.USER_ID + data.CUSTOMER_ID)
+              .emit("availableChatterData", {
+                CHATTER_DATA: response,
+              });
+          })
+          .catch((err) => {
+            console.log("Error", err);
+          });
+        // availableChatters(data).then((response)=>{
+        //   console.log("sdasdas",response);
+
+        // }).catch((err)=>{
+        //   console.log("errr",);
+        // })
       });
     });
   },
